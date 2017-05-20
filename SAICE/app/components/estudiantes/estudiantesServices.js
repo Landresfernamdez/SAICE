18377d9f720974a290cb68bf4434c441cece4a34
Andresf12 <landresf3638@gmail.com>
2017-05-20 12:41:19 -0600

    Falta arreglar el update a nivel de base de datos

--- SAICE/app/components/estudiantes/estudiantesServices.js
+++ SAICE/app/components/estudiantes/estudiantesServices.js
@@ -22,16 +22,14 @@ angular.module('userModule')
 
                 })// si la insercion fue exitosa entra al succes de lo contrario retorna un error departe del servidor
                     .success(function(data){
-                        if (data) {
-                            // Showing errors.
 
+                            alert("insercion exitosa");
+                            callback({success: true});
+                    }).error(function(data) {
+                    //En caso de fallo en la peticion entra en esta funcion
                             alert("Se ha producido un error en la insercion"+data);
                             callback({success: false});
-                        } else {
-                            callback({success: true});
-                            alert("insercion exitosa");
-                        }
-                    });
+                });
             },
             deleteStudents:function(id,callback){
                 $http({
@@ -41,17 +39,17 @@ angular.module('userModule')
 
                 })// si la insercion fue exitosa entra al succes de lo contrario retorna un error departe del servidor
                     .success(function(data){
-                        if (data) {
                             // Showing errors.
-                            console.log(data);
-                            console.log(id);
-                            alert("Se ha producido un error a la hora de eliminar el dato");
-                            callback({success: false});
-                        } else {
                             callback({success: true});
                             alert("se elimino exitosamente ");
-                        }
-                    });
+                            console.log(data);
+                            console.log(id);
+                        
+                    }).error(function(data) {
+                    //En caso de fallo en la peticion entra en esta funcion
+                            alert("Se ha producido un error en la eliminacion"+data);
+                            callback({success: false});
+                });
             },
             updateStudents:function(estudiante,callback){
                 $http({
@@ -61,16 +59,13 @@ angular.module('userModule')
 
                 })// si la insercion fue exitosa entra al succes de lo contrario retorna un error departe del servidor
                     .success(function(data){
-                        if (data) {
-                            // Showing errors.
-                            console.log(data);
-                            alert("Se ha producido un error a la hora de actualizar el estudiante");
-                            callback({success: false});
-                        } else {
-                            callback({success: true});
                             alert("se actualizo exitosamente ");
-                        }
-                    });
+                            callback({success: true});
+                    }).error(function(data) {
+                    //En caso de fallo en la peticion entra en esta funcion
+                            alert("Se ha producido un error en la eliminacion"+data);
+                            callback({success: false});
+                });
             }
         }
         return respuesta;
