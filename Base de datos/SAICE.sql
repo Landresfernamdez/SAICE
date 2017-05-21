61598dcfd9c376b1d44d0dc98594b7d297db8156
sergioDAB <sergioalemanb@gmail.com>
2017-05-20 23:42:49 -0600

    funcion practicas

--- Base de datos/SAICE.sql
+++ Base de datos/SAICE.sql
@@ -313,9 +313,26 @@ CREATE OR REPLACE FUNCTION actualizarEstudiante
 		$BODY$ 
 		LANGUAGE plpgsql;
 
+--- insertar practicas-------------
 
+create or replace function insertar_Practica(
+	p_id_practica varchar(7),
+	p_fecha_inicio date,
+	p_fecha_final date,
+	/*p_nota integer,
+	p_estado char(1), se omiten, se le asignan datos 0,0 y solo se cambian por una funcion NotaPractica*/
+	p_cedula a_cedula,
+	p_id_empresa varchar(7)
+)returns void as
+$BODY$
+Begin
+	raise notice 'Insertando';
+	insert into practicas values (p_id_practica,p_fecha_inicio,p_fecha_final,100,'r',p_cedula,p_id_empresa);	
+	raise notice 'Se inserto Estudiante';
+end $BODY$
+language plpgsql;
 
-
+-----
 
 select * from polizas
 select * from Estudiantes
